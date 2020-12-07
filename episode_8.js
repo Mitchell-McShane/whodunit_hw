1daa8dc3ad65084a83f7185afb4be8df38a2ae01
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2020-12-07 17:30:13 +0000

    episode 8 complete

---
+++ episode_8.js
@@ -0,0 +1,36 @@
+// EPISODE 8
+const scenario = {
+  murderer: 'Mrs. Peacock',
+  room: 'Conservatory',
+  weapon: 'Lead Pipe'
+};
+
+const changeScenario = function() {
+  scenario.murderer = 'Mrs. Peacock';
+  scenario.room = 'Dining Room';
+
+  const plotTwist = function(room) {
+    if (scenario.room === room) {
+      scenario.murderer = 'Colonel Mustard';
+    }
+
+    const unexpectedOutcome = function(murderer) {
+      if (scenario.murderer === murderer) {
+        scenario.weapon = 'Candle Stick';
+      }
+    }
+
+    unexpectedOutcome('Colonel Mustard');
+  }
+
+  plotTwist('Dining Room');
+}
+
+const declareWeapon = function() {
+  return `The weapon is ${scenario.weapon}.`
+}
+
+changeScenario();
+const verdict = declareWeapon();
+console.log('episode 8:',verdict);
+// Prediction: The weapon is: 'Candle Stick'. The changeScenario will change the murder to 'Mrs Peacock' and the room to the 'Dining Room' with the plotTwist changing the murderer to 'Colonel Mustard' and the weapon being changed to the 'Candle Stick'.
